bd3c1f22fdd54fd60b863b94f245cc2ce8535519
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-01-12 23:51:01 -0300

    configuracoes iniciais do nvim

---
+++ nvim/init.vim
@@ -0,0 +1,11 @@
+set number
+syntax on
+
+call plug#begin()
+Plug 'joshdick/onedark.vim'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'preservim/nerdtree'
+Plug 'vim-airline/vim-airline'
+call plug#end()
+
+colorscheme onedark
---
+++ nvim/setup_nvim.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+nvim +PlugInstall
+nvim "+CocInstall coc-rust-analyzer"
