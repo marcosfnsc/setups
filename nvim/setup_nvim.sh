3e1afe7c0ab7138435cb5bdf80f714542d1fa117
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-01-15 00:55:48 -0300

    correcao no comentario

--- nvim/setup_nvim.sh
+++ nvim/setup_nvim.sh
@@ -4,7 +4,7 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# install coc.vim ex
+# install coc.vim extension
 nvim +PlugInstall +q
 nvim "+CocInstall coc-rust-analyzer" +q
 nvim "*CocInstall coc-pyright" +q
