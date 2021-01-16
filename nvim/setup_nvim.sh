9f77d720c9be8fef9a5fe8824cce7c3c0bea94a9
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-01-15 21:50:15 -0300

    adicionado comando para hardlinks

--- nvim/setup_nvim.sh
+++ nvim/setup_nvim.sh
@@ -4,8 +4,13 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+rm $HOME/.config/nvim/init.vim
+ln init.vim $HOME/.config/nvim/init.vim
+
 # install coc.vim extension
-nvim +PlugInstall +q
-nvim "+CocInstall coc-rust-analyzer" +q
-nvim "*CocInstall coc-pyright" +q
+nvim +PlugInstall +qa
+nvim "+CocInstall coc-rust-analyzer" +qa
+echo ok1
+nvim "*CocInstall coc-pyright" +qa
+echo ok2
 
