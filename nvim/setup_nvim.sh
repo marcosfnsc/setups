2229c269dae966c556cacbba650a13c3cfd7442d
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-01-17 23:10:23 -0300

    removido extensoes coc do script

--- nvim/setup_nvim.sh
+++ nvim/setup_nvim.sh
@@ -4,11 +4,9 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-rm $HOME/.config/nvim/init.vim
-ln init.vim $HOME/.config/nvim/init.vim || cp init.vim $HOME/.config/nvim
+rm $HOME/.config/nvim/inita.vim
+ln $PWD/init.vim $HOME/.config/nvim/init.vim || cp $PWD/init.vim $HOME/.config/nvim
 
 # install coc.vim extension
 nvim +PlugInstall +qa
-nvim "+CocInstall coc-rust-analyzer" +qa
-nvim "*CocInstall coc-pyright" +qa
 
