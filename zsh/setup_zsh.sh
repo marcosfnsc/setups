61e32bc47e648fc4ce6bd0237ebb8c349b8db097
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-01-25 22:20:43 -0300

    adicionado if pra verificar se axiste arquivos

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # oh-my-zsh setup
-sudo -u $username sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh/" ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if test -f "$HOME/.zshrc"; then
+  rm $HOME/.zshrc
+fi
 
 ln zshrc $HOME/.zshrc
 
