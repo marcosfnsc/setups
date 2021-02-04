c672bb734a929f194b232add673b4bc59831055e
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-02-03 22:34:22 -0300

    script pra obter os dotfiles

---
+++ dotfiles/backup_dotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp $HOME/.config/dolphinrc          ./dolphinrc
+cp $HOME/.config/kglobalshortcutsrc ./kglobalshortcutsrc
+cp $HOME/.config/konsolerc          ./konsolerc
+cp $HOME/.gitconfig                 ./gitconfig
+
