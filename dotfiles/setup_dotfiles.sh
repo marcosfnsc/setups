0b9c9bcf6e8caca72a50c367952446cd05969513
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-02-06 20:12:12 -0300

    adicionado funcao pra verificar existencia de arquivos antes de remover

--- dotfiles/setup_dotfiles.sh
+++ dotfiles/setup_dotfiles.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-ln dolphinrc          $HOME/.config/dolphinrc          # dolphin
-ln kglobalshortcutsrc $HOME/.config/kglobalshortcutsrc # kglobalshortcutsrc
-ln konsolerc          $HOME/.config/konsolerc          # konsole
-ln gitconfig          $HOME/.gitconfig                 # git
+check_and_rm() {
+  if test -f $1; then
+    rm $1
+  fi 
+}
 
+# dolphin
+check_and_rm $HOME/.config/dolphinrc
+ln dolphinrc $HOME/.config/dolphinrc
+
+# kglobalshortcutsrc
+check_and_rm $HOME/.config/kglobalshortcutsrc
+ln kglobalshortcutsrc $HOME/.config/kglobalshortcutsrc 
+
+# konsole
+check_and_rm $HOME/.config/konsolerc
+ln konsolerc $HOME/.config/konsolerc          
+
+# git
+check_and_rm $HOME/.gitconfig
+ln gitconfig $HOME/.gitconfig                 
