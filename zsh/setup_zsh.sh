feabf8adb1e1d8d44644a8b755c93acb22cd5ade
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-03-19 22:30:24 -0300

    ajustado precedencia do config do zsh

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+if test -f "$HOME/.zshrc"; then
+  rm $HOME/.zshrc
+fi
+
 # oh-my-zsh setup
 if [ ! -d "$HOME/.oh-my-zsh/" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-
-if test -f "$HOME/.zshrc"; then
-  rm $HOME/.zshrc
 fi
 
 ln zshrc $HOME/.zshrc || cp zshrc $HOME/.zshrc
