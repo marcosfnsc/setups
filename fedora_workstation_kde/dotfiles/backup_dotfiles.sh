3717ea4e7233b27a94c7c7ca7fa034682cb2743e
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-03-19 22:49:57 -0300

    script git adiciionado ao setup do fedora

--- fedora_workstation_kde/dotfiles/backup_dotfiles.sh
+++ fedora_workstation_kde/dotfiles/backup_dotfiles.sh
@@ -3,5 +3,3 @@
 cp $HOME/.config/dolphinrc          ./dolphinrc
 cp $HOME/.config/kglobalshortcutsrc ./kglobalshortcutsrc
 cp $HOME/.config/konsolerc          ./konsolerc
-cp $HOME/.gitconfig                 ./gitconfig
-
