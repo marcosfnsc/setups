a0ace838085c5d0d69fd3af331feae3d3e9107cf
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-04-29 08:22:14 -0300

    .adicionado script do git ao termux

--- termux/termux_postinstall.sh
+++ termux/termux_postinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# external storage
+termux-setup-storage
+
 PACKAGES_ADD=(
   clang
   git
@@ -15,8 +18,11 @@ PACKAGES_ADD=(
 pkg install  ${APPS_ADD[@]}
 
 cd ..
-cd nvim && ./setup_nvim.sh && cd ..
-cd zsh  && ./setup_zsh.sh  && cd ..
+cd git_config && ./setup_git     && cd ..
+cd nvim       && ./setup_nvim.sh && cd ..
+cd tmux       && ./setup_tmux.sh && cd ..
+cd zsh        && ./setup_zsh.sh  && cd ..
+cd termux
 
 # install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
