9f3786dd9977428e5ffd35bc1007655fc09a713c
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-07-11 00:01:57 -0300

    flatpak desacoplado

---
+++ flatpak/setup_flatpak.sh
@@ -0,0 +1,12 @@
+# flathub remote
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# install flatpak apps
+FLATPAK_FLATHUB=(
+  com.discordapp.Discord  # Discord
+  com.spotify.Client      # Spotify
+  com.valsesoftware.Steam # Steam
+  org.telegram.desktop    # telegram
+)
+
+flatpak install -y flathub ${FLATPAK_FLATHUB[@]}
