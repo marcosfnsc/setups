9959746b4c9be7972c81294595a4eb252a620e8a
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-07-10 23:36:57 -0300

    config do docker desacoplada

--- fedora_workstation_kde/fedora_kde_postinstall.sh
+++ fedora_workstation_kde/fedora_kde_postinstall.sh
@@ -97,21 +97,6 @@ dnf remove -y ${APPS_REMOVE[@]}
 
 # -----------------------
 
-# setup docker
-
-# install repository
-dnf  install dnf-plugins-core -y
-dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-
-# install docker engine and compose
-dnf install docker-ce docker-ce-cli containerd.io docker-compose -y
-
-# configure to init with system
-systemctl enable docker.service
-systemctl enable containerd.service
-
-# -----------------------
-
 # flathub remote
 sudo -u $username flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
