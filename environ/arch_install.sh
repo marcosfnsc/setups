e60557ef1e259ff2490a057b726157756f43ef82
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-07-19 23:17:27 -0300

    adicionado comandos para criar partições

--- environ/arch_install.sh
+++ environ/arch_install.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+# create partitions
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+  g # clear the disk and create GPT table
+  n # new partition
+  1 # partition number 1
+    # default
+  +500M # 500 MB efi parttion
+  t # change the type partition
+  1 # selected type EFI system
+  n # new partition
+  2 # partion number 2
+    # default
+  +1G # 1 GB boot parttion
+  n # new partition
+  3 # partion number 3
+    # default
+    # full size partition
+  w # write and exit
+EOF
+
 pacstrap /mnt base linux linux-firmware
 
 genfstab -U -p /mnt /mnt/etc/fstab
