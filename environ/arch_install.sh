5bb6e9f4feda1114d9ba87290ac9eceab162078c
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-07-23 11:17:09 -0300

    codigo do arch desacoplado para driblar o arch-chroot

--- environ/arch_install.sh
+++ environ/arch_install.sh
@@ -97,28 +97,8 @@ APPS_INSTALL=(
 )
 pacstrap /mnt base linux linux-firmware ${APPS_INSTALL[@]}
 
-arch-chroot /mnt
+cp arch_install2.sh /mnt
+cd && cp -r setups /mnt
 
-## fuso de brasilia
-#ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+arch-chroot /mnt ./arch_install2.sh
 
-## layout do teclado do sistema
-echo KEYMAP=br-abnt2 >> /etc/vconsole.conf
-
-hostnamectl set-hostname note
-
-# root password
-passwd
-
-# users
-useradd -m marcos
-passwd marcos
-
-pacman -S grub efibootmgr os-prober mtools dosfstools
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable NetworkManager
-
-#other scripts
-./anylinux.sh
