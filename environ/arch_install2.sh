bd88e4d1543e1b609fec66d66401f2bc8c33feaf
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-07-26 09:28:32 -0300

    config do mkinitcpio

--- environ/arch_install2.sh
+++ environ/arch_install2.sh
@@ -15,6 +15,23 @@ passwd
 useradd -m marcos
 passwd marcos
 
+# config mkinitcpio
+HOOKS=$(cat /etc/mkinitcpio.conf | grep HOOKS)
+if [[ $HOOKS == *"keyboard"* ]] ; then
+  sed -e s/keyboard//g -i /etc/mkinitcpio.conf
+fi
+if [[ $HOOKS == *"encrypt"* ]] ; then
+  sed -e s/encrypt//g -i /etc/mkinitcpio.conf
+fi
+if [[ $HOOKS == *"lvm2"* ]] ; then
+  sed -e s/lvm2//g -i /etc/mkinitcpio.conf
+fi
+sed -e "s/autodetect/autodetect keyboard/g" -i /etc/mkinitcpio.conf
+sed -e "s/filesystems/encrypt filesystems/g" -i /etc/mkinitcpio.conf
+sed -e "s/filesystems/lvm2 filesystems/g" -i /etc/mkinitcpio.conf
+sed -e "s/  / /g" -i /etc/mkinitcpio.conf
+mkinitcpio -p linux
+
 pacman -S grub efibootmgr os-prober mtools dosfstools
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
