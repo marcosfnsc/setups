a549022fa768c6bc510862dea652f2833af6446c
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-07-30 22:41:44 -0300

    retirado install dos pacotes via pacstrap para pacman

--- environ/arch_install2.sh
+++ environ/arch_install2.sh
@@ -1,5 +1,42 @@
 #!/usr/bin/env bash
 
+APPS_INSTALL=(
+  alacritty
+  audacity
+  base-devel
+  clang
+  cmake
+  curl
+  ffmpeg
+  flatpak
+  gcc
+  gimp
+  git
+  gparted
+  htop
+  k3b
+  kate
+  kde-applications
+  lvm2
+  neovim
+  networkmanager
+  nodejs
+  okular
+  openssh
+  plasma
+  qbittorrent
+  screenfetch
+  sddm
+  sudo
+  the_silver_searcher
+  tmux
+  tree
+  xorg
+  zsh
+)
+
+pacman -S ${APPS_INSTALL[@]}
+
 ## fuso de brasilia
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc --utc
