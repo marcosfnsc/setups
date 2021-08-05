be8423b85ada5aae7a48a99264156d870e582e59
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-08-05 19:02:37 -0300

    corrigo config do zram
    adicionado zram em modules em mkinitcpio e movido essa confg para o
    scropt2 de instalacao do arch linux

--- environ/arch_install3.sh
+++ environ/arch_install3.sh
@@ -38,16 +38,7 @@ pacman -S --needed ${APPS_INSTALL[@]}
 
 hostnamectl set-hostname note
 
-## conf zram
-echo 'zram' > /etc/modules-load.d/zram.conf
-echo 'options zram num_devices=1' > /etc/modprobe.d/zram.conf
-
-RAM_SIZE=$(free -h | sed -n '2 p' | awk '{print $2}')
-echo "KERNEL==”zram0″, ATTR{disksize}=”$RAM_SIZE” RUN=”/usr/bin/mkswap /dev/zram0″, TAG+=”systemd”" > /etc/udev/rules.d/99-zram.rules
-echo "/dev/zram0 none swap defaults 0 0" >> /etc/fstab
-
 systemctl enable sddm
-systemctl enable zram
 
 #other scripts
 cd setups/environ && ./anylinux.sh
