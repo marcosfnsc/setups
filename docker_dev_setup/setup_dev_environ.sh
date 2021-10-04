7c1b03e991dd7e46476747ec54ac53b4ae26ef7e
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-10-03 22:16:49 -0300

    removido comandos sudo

--- docker_dev_setup/setup_dev_environ.sh
+++ docker_dev_setup/setup_dev_environ.sh
@@ -2,8 +2,8 @@
 
 REPO_ROOT=$PWD/..
 
-cd $REPO_ROOT/fzf                && sudo -u $_USERNAME ./setup_fzf.sh
-cd $REPO_ROOT/git_config         && sudo -u $_USERNAME ./setup_git.sh
-cd $REPO_ROOT/nvim               && sudo -u $_USERNAME ./setup_nvim.sh
-cd $REPO_ROOT/tmux               && sudo -u $_USERNAME ./setup_tmux.sh
+cd $REPO_ROOT/fzf                && ./setup_fzf.sh
+cd $REPO_ROOT/git_config         && ./setup_git.sh
+cd $REPO_ROOT/nvim               && ./setup_nvim.sh
+cd $REPO_ROOT/tmux               && ./setup_tmux.sh
 #cd $REPO_ROOT/zsh                && sudo -u $_USERNAME ./setup_zsh.sh
