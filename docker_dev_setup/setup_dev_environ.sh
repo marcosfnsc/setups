b1016103257855e5267b6a6ae1c45346798ce979
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-10-09 19:08:14 -0300

    removido config personalizada do zsh

--- docker_dev_setup/setup_dev_environ.sh
+++ docker_dev_setup/setup_dev_environ.sh
@@ -2,25 +2,8 @@
 
 REPO_ROOT=$PWD/..
 
-cd $REPO_ROOT/fzf                && ./setup_fzf.sh
-cd $REPO_ROOT/git_config         && ./setup_git.sh
-cd $REPO_ROOT/nvim               && ./setup_nvim.sh
-cd $REPO_ROOT/tmux               && ./setup_tmux.sh
-
-if test -f "$HOME/.zshrc"; then
-  rm $HOME/.zshrc
-fi
-
-# oh-my-zsh setup
-if [ ! -d "$HOME/.oh-my-zsh/" ]; then
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-fi
-
-{
-  ln $REPO_ROOT/zsh/zshrc $HOME/.zshrc 2>/dev/null
-} || {
-  echo "erro ao criar link fisico, copiando arquivo ..."
-  cp $REPO_ROOT/zsh/zshrc $HOME/.zshrc
-}
-
-echo "exec zsh" > $HOME/.bashrc
+cd $REPO_ROOT/fzf        && ./setup_fzf.sh
+cd $REPO_ROOT/git_config && ./setup_git.sh
+cd $REPO_ROOT/nvim       && ./setup_nvim.sh
+cd $REPO_ROOT/tmux       && ./setup_tmux.sh
+cd $REPO_ROOT/zsh        && ./setup_zsh.sh
