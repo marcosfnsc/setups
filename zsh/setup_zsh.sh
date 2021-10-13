8d10060e4d97d6b55b21c9b4e62f56c5456fb1d3
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-10-13 17:42:21 -0300

    função pessoal separada do zshrc

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -4,6 +4,10 @@ if test -f "$HOME/.zshrc"; then
   rm $HOME/.zshrc
 fi
 
+if test -f "$HOME/.shell_functions"; then
+  rm $HOME/.zshrc
+fi
+
 # oh-my-zsh setup
 if [ ! -d "$HOME/.oh-my-zsh/" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -14,4 +18,11 @@ fi
 } || {
   echo "erro ao criar link fisico, copiando arquivo ..."
   cp zshrc $HOME/.zshrc
+}
+
+{
+  ln shell_functions $HOME/.shell_functions 2>/dev/null
+} || {
+  echo "erro ao criar link fisico, copiando arquivo ..."
+  cp zshrc $HOME/.shell_functions
 }
