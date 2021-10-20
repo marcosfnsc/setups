3307f8677e2bc5ea951070b695e84d75654f1a85
Marcos Fonseca <marco12.fonseca@hotmail.com>
2021-10-20 08:42:51 -0300

    adicionado comentarios sobre config TRIM

--- environ/arch_install2.sh
+++ environ/arch_install2.sh
@@ -59,7 +59,7 @@ pacman -S --needed grub efibootmgr os-prober mtools dosfstools
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 
 UUID_SDA3=$(lsblk -no NAME,UUID /dev/sda3|head -n 1 | awk '{print $2}')
-CRYPT_DEVICE="cryptdevice=UUID=$UUID_SDA3:cryptlvm"
+CRYPT_DEVICE="cryptdevice=UUID=$UUID_SDA3:cryptlvm" # add :allow-discards to enable TRIM commands
 ROOT_DEVICE="root=/dev/lvgroup/root"
 RESUME_DEVICE="resume=/dev/lvgroup/swap" # for hibernation
 sed -e "s;GRUB_CMDLINE_LINUX=\"[[:print:]]*\";GRUB_CMDLINE_LINUX=\"$CRYPT_DEVICE $ROOT_DEVICE $RESUME_DEVICE\";g" -i etc/default/grub
