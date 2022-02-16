0f61088344357701a44439cba814c989ac70e5b3
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-02-15 22:32:31 -0300

    corrigido bug no script do kitty

--- kitty/setup_kitty.sh
+++ kitty/setup_kitty.sh
@@ -1,10 +1,10 @@
 
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/kitty
+mkdir -p $HOME/.config/kitty
 
-if test -f "~/.config/kitty/kitty.conf"; then
-  rm ~/.config/kitty/kitty.conf
+if test -f "$HOME/.config/kitty/kitty.conf"; then
+  rm $HOME/.config/kitty/kitty.conf
 fi
 
-ln kitty.conf ~/.config/kitty/kitty.conf
+ln kitty.conf $HOME/.config/kitty/kitty.conf
