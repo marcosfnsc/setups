7e41bc5887f190a8267f0468625ae2cfed8a0d25
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-03-05 18:55:20 -0300

    reduzido alguns codigos do setup nvim

--- nvim/setup_nvim.sh
+++ nvim/setup_nvim.sh
@@ -6,25 +6,17 @@ if [ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ]; then
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
-mkdir -p $HOME/.config/nvim
-if test -f "$HOME/.config/nvim/init.lua"; then
-  rm $HOME/.config/nvim/init.lua
-fi
 {
-  ln init.lua $HOME/.config/nvim/init.lua 2>/dev/null
+  ln -f init.lua $HOME/.config/nvim/init.lua 2>/dev/null
 } || {
   echo "erro ao criar link fisico, copiando arquivo ..."
-  cp init.lua $HOME/.config/nvim
+  cp -f init.lua $HOME/.config/nvim
 }
 
 # install vim-plug extension
 nvim +PlugInstall +qa
 
 # settings coc
-if test -f "$HOME/.config/nvim/coc-settings.json"; then
-  rm $HOME/.config/nvim/coc-settings.json
-fi
-
 echo "{
   \"explorer.width\": 30,
   \"explorer.file.showHiddenFiles\": true," > $HOME/.config/nvim/coc-settings.json
