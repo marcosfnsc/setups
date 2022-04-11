c197230b67ab58fa86f751cb660eb6446cadbb99
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-04-11 17:28:42 -0300

    melhorias no script de config do flutter

--- flutter/setup_flutter.sh
+++ flutter/setup_flutter.sh
@@ -1,4 +1,7 @@
 git clone --depth 1 --branch stable https://github.com/flutter/flutter.git $HOME/.flutter
 
 flutter precache
-flutter config --no-analytics
+flutter config --no-analytics # desativar coleta de dados do google
+
+#flutter config --enable-linux-desktop
+#pacman -Sy --needed clang cmake ninja gtk3 pkgconfig xz # depedencias para compilar no archlinux
