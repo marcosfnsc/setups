9209819c4f188cab1896b8b3789144e41f6675da
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-04-16 17:35:28 -0300

    configuração dividido em varios arquivos *.lua e configurado o script

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,57 @@
+
+vim.cmd([[
+call plug#begin()
+"Plug 'HerringtonDarkholme/yats.vim' "syntax highlighting for TypeScript, includes react project
+"Plug 'maxmellon/vim-jsx-pretty' "syntax highlighting for javascript, includes react project
+Plug 'akinsho/bufferline.nvim', { 'tag': '*' }
+Plug 'bfrg/vim-cpp-modern' "syntax highlighting c/c++
+Plug 'christoomey/vim-tmux-navigator'
+Plug 'dense-analysis/ale', {'for': ['python']}
+Plug 'editorconfig/editorconfig-vim'
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+Plug 'kevinoid/vim-jsonc'
+Plug 'lewis6991/gitsigns.nvim',
+Plug 'lukas-reineke/indent-blankline.nvim'
+Plug 'navarasu/onedark.nvim'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'nvim-lua/plenary.nvim'
+Plug 'tpope/vim-fugitive'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'vim-python/python-syntax'
+call plug#end()
+]])
+
+vim.cmd([[ colorscheme onedark]]) -- theme
+vim.cmd([[let g:indentLine_setColors = 0]])
+vim.g['airline_theme'] = 'deus' --
+vim.g['ale_linters'] = {python = {'pylint'}} -- ale config
+vim.g['python_highlight_all'] = 1 -- enable highlight for python
+vim.g['coc_global_extensions'] = {
+    'coc-emmet',
+    'coc-explorer',
+    'coc-html',
+    'coc-jedi',
+    'coc-pairs',
+    'coc-rust-analyzer',
+    'coc-sh',
+    'coc-tsserver',
+}
+--    'coc-clangd',
+--    'coc-java',
+--    'coc-css',
+
+require('bufferline').setup {
+  options = {
+    show_buffer_close_icons = false,
+  }
+}
+require('gitsigns').setup()
+
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#353d44 gui=nocombine]]
+require("indent_blankline").setup {
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+    },
+}
--- nvim/setup_nvim.sh
+++ nvim/setup_nvim.sh
@@ -6,11 +6,16 @@ if [ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ]; then
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
+mkdir -p $HOME/.config/nvim/lua
 {
-  ln -f init.lua $HOME/.config/nvim/init.lua 2>/dev/null
+  ln -f init.lua         $HOME/.config/nvim/init.lua         2>/dev/null
+  ln -f lua/mappings.lua $HOME/.config/nvim/lua/mappings.lua 2>/dev/null
+  ln -f lua/plugins.lua  $HOME/.config/nvim/lua/plugins.lua  2>/dev/null
 } || {
   echo "erro ao criar link fisico, copiando arquivo ..."
-  cp -f init.lua $HOME/.config/nvim
+  cp -f init.lua         $HOME/.config/nvim
+  cp -f lua/mappings.lua $HOME/.config/nvim/lua
+  cp -f lua/plugins.lua  $HOME/.config/nvim/lua
 }
 
 # install vim-plug extension
