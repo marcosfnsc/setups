7d7f99fe9081eaae517fe49dac1970c0e764321a
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-05-14 00:12:22 -0300

    terminado setup por enquanto com apenas uma conta

--- neomutt/setup_neomutt.sh
+++ neomutt/setup_neomutt.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
 DIRECTORY_CONFIG=$HOME/.config/neomutt
 mkdir -p $DIRECTORY_CONFIG/{accounts,cache}
 mkdir -p $DIRECTORY_CONFIG/cache/{headers,bodies}
+
+{
+  ln -f neomuttrc $HOME/.config/neomutt/neomuttrc 2>/dev/null
+} || {
+  echo "erro ao criar link fisico, copiando arquivo ..."
+  cp -f neomuttrc $HOME/.config/neomutt/neomuttrc
+}
