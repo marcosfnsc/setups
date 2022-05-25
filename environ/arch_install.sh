2511d27854e20ea518e2edf63e46f599090b49b2
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-05-25 16:56:07 -0300

    trocado codigo de particionamento usando gambiarram com fdisk para versão parted com flag de script

--- environ/arch_install.sh
+++ environ/arch_install.sh
@@ -3,20 +3,12 @@
 # dicas-> loadkeys br-abnt2
 
 # create partitions
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
-  g # clear the disk and create GPT table
-  n # new partition
-  1 # partition number 1
-    # default
-  +500M # 500 MB efi parttion
-  t # change the type partition
-  1 # selected type EFI system
-  n # new partition
-  2 # partion number 2
-    # default
-    # full size partition
-  w # write and exit
-EOF
+parted -s /dev/sda \
+  mklabel gpt \
+  mkpart primary 1MiB 500MiB \
+  mkpart primary 500MiB 100% \
+  --align optimal \
+  align-check min 1
 
 ## config luks2
 modprobe dm-crypt
