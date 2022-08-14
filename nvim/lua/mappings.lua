c08016885711b5feebe299399f3fcd920b405a4d
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-08-14 18:31:21 -0300

    adicionado fix para o autocomplete do coc

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -14,4 +14,9 @@ inoremap jj <Esc>
 
 "save buffer
 noremap <Leader>s :update<CR>
+
+
+"hack for coc autocomplete bug
+inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
 ]])
