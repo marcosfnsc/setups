0908cf77ce0662bb6a5e0b40e95a13bb22b59258
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-08-17 17:00:06 -0300

    config do tmux dividida em varios arquivos

--- tmux/setup_tmux.sh
+++ tmux/setup_tmux.sh
@@ -2,7 +2,9 @@
 
 {
   ln -f tmux.conf $HOME/.tmux.conf 2>/dev/null
+  ln -f tmux_theme.conf $HOME/.tmux_theme.conf 2>/dev/null
 } || {
   echo "erro ao criar link fisico, copiando arquivo ..."
   cp -f tmux.conf $HOME/.tmux.conf
+  cp -f tmux_theme.conf $HOME/.tmux_theme.conf
 }
