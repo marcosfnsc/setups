4d41e53ff4e3aea7df422440710a5072948d7c16
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-08-18 00:36:18 -0300

    criado arquivos para abrigar config dos plugins

--- tmux/setup_tmux.sh
+++ tmux/setup_tmux.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 {
-  ln -f tmux.conf $HOME/.tmux.conf 2>/dev/null
-  ln -f tmux_theme.conf $HOME/.tmux_theme.conf 2>/dev/null
+  ln -f tmux.conf         $HOME/.tmux.conf         2>/dev/null
+  ln -f tmux_plugins.conf $HOME/.tmux_plugins.conf 2>/dev/null
+  ln -f tmux_theme.conf   $HOME/.tmux_theme.conf   2>/dev/null
 } || {
   echo "erro ao criar link fisico, copiando arquivo ..."
-  cp -f tmux.conf $HOME/.tmux.conf
-  cp -f tmux_theme.conf $HOME/.tmux_theme.conf
+  cp -f tmux.conf         $HOME/.tmux.conf
+  cp -f tmux_plugins.conf $HOME/.tmux_plugins.conf
+  cp -f tmux_theme.conf   $HOME/.tmux_theme.conf
 }
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  ~/.tmux/plugins/tpm/bin/install_plugins
+fi
