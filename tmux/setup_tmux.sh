357e362bbdc5c84f64cdb5dbcb13e4b2983506fa
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-08-18 20:06:41 -0300

    removido codigo referente ao arquivo de tema

--- tmux/setup_tmux.sh
+++ tmux/setup_tmux.sh
@@ -3,12 +3,10 @@
 {
   ln -f tmux.conf         $HOME/.tmux.conf         2>/dev/null
   ln -f tmux_plugins.conf $HOME/.tmux_plugins.conf 2>/dev/null
-  ln -f tmux_theme.conf   $HOME/.tmux_theme.conf   2>/dev/null
 } || {
   echo "erro ao criar link fisico, copiando arquivo ..."
   cp -f tmux.conf         $HOME/.tmux.conf
   cp -f tmux_plugins.conf $HOME/.tmux_plugins.conf
-  cp -f tmux_theme.conf   $HOME/.tmux_theme.conf
 }
 
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
