0f1ee072e3e3e099957685f9902aaf8cf3853860
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-08-21 22:58:21 -0300

    trocado coc.nvim por varios plugins de lsp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,11 +1,17 @@
 
 vim.cmd([[
 call plug#begin()
-Plug 'OmniSharp/omnisharp-vim'
 Plug 'akinsho/bufferline.nvim', { 'tag': '*' }
 Plug 'christoomey/vim-tmux-navigator'
 Plug 'dense-analysis/ale', {'for': ['python']}
 Plug 'editorconfig/editorconfig-vim'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/vim-vsnip'
+Plug 'jose-elias-alvarez/typescript.nvim'
 Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
 Plug 'junegunn/fzf.vim'
 Plug 'kevinoid/vim-jsonc'
@@ -13,37 +19,30 @@ Plug 'kyazdani42/nvim-tree.lua'
 Plug 'lewis6991/gitsigns.nvim',
 Plug 'lukas-reineke/indent-blankline.nvim'
 Plug 'navarasu/onedark.nvim'
-Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'neovim/nvim-lspconfig'
 Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 Plug 'nvim-treesitter/nvim-treesitter-context'
+Plug 'ray-x/lsp_signature.nvim'
+Plug 'simrat39/rust-tools.nvim'
 Plug 'tpope/vim-fugitive'
 Plug 'tpope/vim-surround'
 Plug 'vim-airline/vim-airline'
 Plug 'vim-airline/vim-airline-themes'
+Plug 'williamboman/mason-lspconfig.nvim'
+Plug 'williamboman/mason.nvim'
+Plug 'windwp/nvim-autopairs'
 call plug#end()
 ]])
 
 vim.cmd([[ colorscheme onedark]]) -- theme
 vim.g['airline_theme'] = 'deus' --
 vim.g['ale_linters'] = {python = {'pylint'}} -- ale config
-vim.g['coc_global_extensions'] = {
-    'coc-emmet',
-    'coc-html',
-    'coc-pairs',
-    'coc-rust-analyzer',
-    'coc-tsserver',
-}
---    'coc-clangd',
---    'coc-css',
---    'coc-java',
---    'coc-jedi',
 
 require('bufferline').setup {
     options = {
         show_buffer_close_icons = false,
     }
 }
-require('gitsigns').setup()
 
 vim.cmd [[highlight IndentBlanklineIndent1 guifg=#353d44 gui=nocombine]]
 require('indent_blankline').setup {
@@ -73,6 +72,22 @@ require('nvim-treesitter.configs').setup {
     },
 }
 
+require('treesitter-context').setup{
+    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+        default = {
+            'class',
+            'function',
+            'method',
+        },
+    },
+    zindex = 20, -- The Z-index of the context window
+    mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+    separator = nil, -- Separator between context and content. Should be a single character string, like '-'.
+}
+
 require('nvim-tree').setup({
     sort_by = 'case_sensitive',
     view = {
@@ -91,18 +106,50 @@ require('nvim-tree').setup({
     },
 })
 
-require('treesitter-context').setup{
-    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-    trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-        default = {
-            'class',
-            'function',
-            'method',
-        },
+local cmp = require'cmp'
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body)
+        end,
     },
-    zindex = 20, -- The Z-index of the context window
-    mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
-    separator = nil, -- Separator between context and content. Should be a single character string, like '-'.
+    mapping = {
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
+    },
+    sources = {
+        { name = 'buffer' },
+        { name = 'nvim_lsp' },
+    }
 }
+
+require('mason').setup()
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'rust_analyzer',
+        'clangd',
+        'pyright',
+        'sumneko_lua',
+    },
+    automatic_installation = true,
+})
+
+cfg = {}
+require('lsp_signature').setup(cfg)
+
+require('gitsigns').setup()
+require('lspconfig').clangd.setup{}
+require('lspconfig').pyright.setup{}
+require('nvim-autopairs').setup({ map_cr = true })
+require('rust-tools').setup()
+require('typescript').setup()
