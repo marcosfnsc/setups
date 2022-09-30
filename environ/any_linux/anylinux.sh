6f21a9195f7efb5ad95593b9f890577cd728d76b
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-09-30 16:07:15 -0300

    corrigido caminhos dos scripts e removido linhas desativadas

--- environ/any_linux/anylinux.sh
+++ environ/any_linux/anylinux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPO_ROOT=$PWD/..
+REPO_ROOT=$PWD/../..
 _USERNAME=marcos
 
 # config sudoers
@@ -13,24 +13,19 @@ hostnamectl set-hostname note
 # config swap
 echo "vm.swappiness=10" >> /etc/sysctl.conf
 
-# install pyenv
-#sudo -u $_USERNAME curl https://pyenv.run | bash
-
 # rust environpment
 sudo -u $_USERNAME curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-# -----------------------
-
 # run other scripts
-cd $REPO_ROOT/kitty              && sudo -u $_USERNAME ./setup_kitty.sh
-cd $REPO_ROOT/environ/dotfiles   && sudo -u $_USERNAME ./setup_dotfiles.sh
-cd $REPO_ROOT/environ/modprobe.d &&                    ./setup_modprobe.sh
-cd $REPO_ROOT/flatpak            && sudo -u $_USERNAME ./setup_flatpak.sh
-cd $REPO_ROOT/fzf                && sudo -u $_USERNAME ./setup_fzf.sh
-cd $REPO_ROOT/git_config         && sudo -u $_USERNAME ./setup_git.sh
-cd $REPO_ROOT/nvim               && sudo -u $_USERNAME ./setup_nvim.sh
-cd $REPO_ROOT/tmux               && sudo -u $_USERNAME ./setup_tmux.sh
-cd $REPO_ROOT/zsh                && sudo -u $_USERNAME ./setup_zsh.sh
+cd $REPO_ROOT/environ/any_linux/kde_dotfiles && sudo -u $_USERNAME ./setup_dotfiles.sh
+cd $REPO_ROOT/environ/any_linux/modprobe.d   &&                    ./setup_modprobe.sh
+cd $REPO_ROOT/flatpak                        && sudo -u $_USERNAME ./setup_flatpak.sh
+cd $REPO_ROOT/fzf                            && sudo -u $_USERNAME ./setup_fzf.sh
+cd $REPO_ROOT/git_config                     && sudo -u $_USERNAME ./setup_git.sh
+cd $REPO_ROOT/kitty                          && sudo -u $_USERNAME ./setup_kitty.sh
+cd $REPO_ROOT/nvim                           && sudo -u $_USERNAME ./setup_nvim.sh
+cd $REPO_ROOT/tmux                           && sudo -u $_USERNAME ./setup_tmux.sh
+cd $REPO_ROOT/zsh                            && sudo -u $_USERNAME ./setup_zsh.sh
 
 sudo -u $_USERNAME chsh -s $(which zsh)
 
