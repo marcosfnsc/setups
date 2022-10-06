0f3436bb1c55b62f1b65c0c9c42ef71686ba259b
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-10-06 10:12:23 -0300

    criado script para ativação da webcam

--- environ/any_linux/modprobe.d/enable_webcam.sh
+++ environ/any_linux/modprobe.d/enable_webcam.sh
@@ -4,5 +4,4 @@ if test -f "/etc/modprobe.d/webcam-blacklist.conf"; then
   rm /etc/modprobe.d/webcam-blacklist.conf
 fi
 
-modprobe -r uvcvideo
-cp webcam-blacklist.conf /etc/modprobe.d/webcam-blacklist.conf
+modprobe uvcvideo
