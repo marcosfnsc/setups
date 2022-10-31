bc197c76fec54e160cb84f475d566addf11c27d2
Marcos Fonseca <marco12.fonseca@hotmail.com>
2022-10-31 20:33:42 -0300

    adicionado lutris pra instalar via flatpak

--- flatpak/setup_flatpak.sh
+++ flatpak/setup_flatpak.sh
@@ -14,6 +14,7 @@ FLATPAK_FLATHUB=(
   com.google.AndroidStudio  # Android Studio
   com.spotify.Client        # Spotify
   com.valsesoftware.Steam   # Steam
+  net.lutris.Lutris         # lutris
   org.audacityteam.Audacity # Audacity
   org.telegram.desktop      # telegram
 )
