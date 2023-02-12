6d52c258ad7ebe1e5967274cc252645521a09445
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-02-12 19:09:01 -0300

    atualizado nome do LSP da linguagem lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -176,8 +176,8 @@ return require('packer').startup(function(use)
         'neovim/nvim-lspconfig',
         config = function()
             require('lspconfig').clangd.setup{}
+            require('lspconfig').lua_ls.setup{}
             require('lspconfig').pyright.setup{}
-            require('lspconfig').sumneko_lua.setup{}
             require('lspconfig').texlab.setup{}
             require('lspconfig').tsserver.setup{}
         end
