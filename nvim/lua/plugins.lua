b73888e452f7c9a1c175f5d188dce6dc54ebf5b4
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-03-05 19:50:09 -0300

    fixado estado da versão do plugin nvim-tree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -198,6 +198,7 @@ return require('packer').startup(function(use)
     }
     use {
         'kyazdani42/nvim-tree.lua',
+        commit = '8b8d457e07d279976a9baac6bbff5aa036afdc5f', -- adiar atualização da nova api de mapemanetos
         config = function()
             require('nvim-tree').setup({
 
