fa4f81b92a1adb90ce2859851e0e72527fbc5e64
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-04-14 23:55:53 -0300

    adicionado instrução pra parar script ao detectar erro

--- nvim/setup_nvim.sh
+++ nvim/setup_nvim.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 # install packer.nvim
 if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" ]; then
   git clone --depth 1 https://github.com/wbthomason/packer.nvim \
