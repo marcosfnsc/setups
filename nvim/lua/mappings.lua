b0168a81a83d052251359888ce90d7344fda7608
Marcos Fonseca <marco12.fonseca@hotmail.com>
2023-11-06 22:25:55 -0300

    adicionado alguns mapeamentos faceis de lembrar pra redimencionar janelas

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,10 +8,15 @@ local mappings = {
     { 'n', '<leader>ca', [[<Cmd>lua vim.lsp.buf.code_action()<CR>]], opts },              -- call code actions
     { 'n', '<leader>gd', [[<Cmd>lua vim.lsp.buf.definition()<CR>]], opts },               -- jump to definition
     { 'n', '<leader>gt', [[<Cmd>lua vim.lsp.buf.hover()<CR>]], opts },                    -- show hover
-    { 'n', '<leader>L', [[<Cmd>call clearmatches()<CR>]], opts },                         -- clear arbitrary lines selected
-    { 'n', '<leader>l', [[<Cmd>call matchadd('Search', '\%'.line('.').'l')<CR>]], opts }, -- select arbitrary lines
+    { 'n', '<leader>V', [[<Cmd>call clearmatches()<CR>]], opts },                         -- clear arbitrary lines selected
+    { 'n', '<leader>v', [[<Cmd>call matchadd('Search', '\%'.line('.').'l')<CR>]], opts }, -- select arbitrary lines
     { 'n', '<leader>s', [[<Cmd>update<CR>]], opts },                                      -- save buffer
     { 'n', '<space>e', [[<Cmd>NvimTreeToggle<CR>]], opts },                               -- file explorer
+
+    { 'n', '<leader>h', [[<Cmd>vertical resize -1<CR>]], opts },
+    { 'n', '<leader>j', [[<Cmd>resize +1<CR>]], opts },
+    { 'n', '<leader>k', [[<Cmd>resize -1<CR>]], opts },
+    { 'n', '<leader>l', [[<Cmd>vertical resize +1<CR>]], opts },
 }
 
 for _, map in pairs(mappings) do
