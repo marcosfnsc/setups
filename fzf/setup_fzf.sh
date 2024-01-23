7c9ec5d2885f746a4af042ae3d67cf0cc85533ce
Marcos Fonseca <marco12.fonseca@hotmail.com>
2024-01-23 19:54:55 -0300

    configurado para atualizad uma instaçaão existente

--- fzf/setup_fzf.sh
+++ fzf/setup_fzf.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+REPO_URL="https://github.com/junegunn/fzf.git"
+REPO_PATH="$HOME/.fzf"
+git -C $REPO_PATH pull 2>/dev/null || git clone --depth 1 $REPO_URL $REPO_PATH
 $HOME/.fzf/install --all
 
 echo "setup fzf sucess"
