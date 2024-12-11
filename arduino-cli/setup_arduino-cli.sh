4e69f3cb7129b290d9240b6c93c86d798f70fbb4
Marcos Fonseca <marco12.fonseca@hotmail.com>
2024-12-11 17:32:16 -0300

    configurado condição para so instalar o binario caso ja não esteja instalado

--- arduino-cli/setup_arduino-cli.sh
+++ arduino-cli/setup_arduino-cli.sh
@@ -4,14 +4,17 @@ set -o errexit
 
 # install arduino-cli
 ARDUINO_PATH=~/.arduino-cli_bin
-mkdir $ARDUINO_PATH
-curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=$ARDUINO_PATH sh
+if [ ! -d ~/.arduino-cli_bin/ ]; then
+  mkdir $ARDUINO_PATH
+  curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=$ARDUINO_PATH sh
+fi
 
 # install core plataform support
 $ARDUINO_PATH/arduino-cli config init
 $ARDUINO_PATH/arduino-cli config set board_manager.additional_urls \
   http://arduino.esp8266.com/stable/package_esp8266com_index.json \
   https://dl.espressif.com/dl/package_esp32_index.json
+$ARDUINO_PATH/arduino-cli config set library.enable_unsafe_install true
 $ARDUINO_PATH/arduino-cli core update-index
 $ARDUINO_PATH/arduino-cli core install arduino:avr
 $ARDUINO_PATH/arduino-cli core install esp32:esp32
@@ -21,7 +24,6 @@ $ARDUINO_PATH/arduino-cli core install esp8266:esp8266
 ## AsyncTCP for esp32
 ## ESPAsyncTCP for esp8266
 ## ESPAsyncUDP for esp8266
-$ARDUINO_PATH/arduino-cli config set library.enable_unsafe_install true
 $ARDUINO_PATH/arduino-cli lib install "Arduinojson@7.0.4"
 $ARDUINO_PATH/arduino-cli lib install "AsyncTCP@1.1.4"
 $ARDUINO_PATH/arduino-cli lib install "ESPAsyncTCP@1.2.4"
