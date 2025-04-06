d895b7036d1fc95c5053fa09b6ac643ec7e5a0b6
Marcos Fonseca <marco12.fonseca@hotmail.com>
2025-04-05 22:33:36 -0300

    corrigido atalho padrão substituido

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -287,9 +287,20 @@ require('lazy').setup({
     {
         'nvim-tree/nvim-tree.lua',
         config = function()
-            require('nvim-tree').setup()
-            local api = require('nvim-tree.api')
-            vim.keymap.set('n', 'l', api.node.open.edit)
+            local function my_on_attach(bufnr)
+              local api = require "nvim-tree.api"
+
+              local function opts(desc)
+                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+              end
+
+              api.config.mappings.default_on_attach(bufnr)
+              vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+            end
+
+            require("nvim-tree").setup {
+              on_attach = my_on_attach,
+            }
         end
     },
     {
