ddb049b34daa5b51831b564af0913b2c8e53bee7
Marcos Fonseca <marco12.fonseca@hotmail.com>
2025-05-14 21:08:18 -0300

    adicionado comandos nix

--- nix/setup_nix.sh
+++ nix/setup_nix.sh
@@ -5,3 +5,9 @@ ln -f nix.conf $HOME/.config/nix/nix.conf
 
 mkdir -p $HOME/.config/home-manager
 ln -f home.nix $HOME/.config/home-manager/home.nix
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+
+home-manager switch
