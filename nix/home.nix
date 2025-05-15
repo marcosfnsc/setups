5cab5206f7eec3fb230bee136cf678d89fa4cd84
Marcos Fonseca <marco12.fonseca@hotmail.com>
2025-05-14 21:04:46 -0300

    adicionado configuração do home-manager

---
+++ nix/home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "marcos";
+  home.homeDirectory = "/home/marcos";
+  home.stateVersion = "24.11";
+
+  home.packages = [
+    pkgs.yt-dlp
+  ];
+
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
--- nix/setup_nix.sh
+++ nix/setup_nix.sh
@@ -2,3 +2,6 @@
 
 mkdir -p $HOME/.config/nix
 ln -f nix.conf $HOME/.config/nix/nix.conf
+
+mkdir -p $HOME/.config/home-manager
+ln -f home.nix $HOME/.config/home-manager/home.nix
