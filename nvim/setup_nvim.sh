2800bef6cfc881054764a971a7ef6d14fea31fae
Marcos Fonseca <marco12.fonseca@hotmail.com>
2025-05-26 21:59:17 -0300

    script refatorado

--- nvim/setup_nvim.sh
+++ nvim/setup_nvim.sh
@@ -2,16 +2,18 @@
 
 set -o errexit
 
-mkdir -p $HOME/.config/nvim/lua
-{
-  ln -f init.lua         $HOME/.config/nvim/init.lua         2>/dev/null
-  ln -f lua/mappings.lua $HOME/.config/nvim/lua/mappings.lua 2>/dev/null
-  ln -f lua/plugins.lua  $HOME/.config/nvim/lua/plugins.lua  2>/dev/null
-} || {
-  echo "erro ao criar link fisico, copiando arquivo ..."
-  cp -f init.lua         $HOME/.config/nvim
-  cp -f lua/mappings.lua $HOME/.config/nvim/lua
-  cp -f lua/plugins.lua  $HOME/.config/nvim/lua
+symbol_link_or_copy() {
+  {
+    ln -f $1 $2 2>/dev/null
+  } || {
+    echo "erro ao criar link fisico do arquivo $1, copiando arquivo ..."
+    cp -f $1 $2
+  }
 }
+
+mkdir -p $HOME/.config/nvim/lua
+symbol_link_or_copy init.lua         $HOME/.config/nvim/init.lua
+symbol_link_or_copy lua/mappings.lua $HOME/.config/nvim/lua/mappings.lua
+symbol_link_or_copy lua/plugins.lua  $HOME/.config/nvim/lua/plugins.lua
 
 echo "setup nvim sucess"
