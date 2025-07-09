f4e79d73178b8571c1a3c3706db086b30ea6ed0a
Marcos Fonseca <marco12.fonseca@hotmail.com>
2025-07-09 19:03:36 -0300

    melhorado script de configuração

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 # oh-my-zsh setup
 if [ ! -d "$HOME/.oh-my-zsh/" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-{
-  ln -f zshrc $HOME/.zshrc 2>/dev/null
-} || {
-  echo "erro ao criar link fisico, copiando arquivo ..."
-  cp -f zshrc $HOME/.zshrc
+symbol_link_or_copy() {
+  {
+    ln -f $1 $2 2>/dev/null
+  } || {
+    echo "erro ao criar link fisico do arquivo $1, copiando arquivo ..."
+    cp -f $1 $2
+  }
 }
 
-{
-  ln -f shell_functions $HOME/.shell_functions 2>/dev/null
-} || {
-  echo "erro ao criar link fisico, copiando arquivo ..."
-  cp -f shell_functions $HOME/.shell_functions
-}
+symbol_link_or_copy shell_functions $HOME/.shell_functions
+symbol_link_or_copy zshrc           $HOME/.zshrc
 
 echo "setup zsh sucess"
