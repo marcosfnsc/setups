55d4a8e870c086421dc46cdee16911431e4d98b1
Marcos Fonseca <marco12.fonseca@hotmail.com>
2025-08-10 20:12:48 -0300

    corrigido identação

--- nix/flake.nix
+++ nix/flake.nix
@@ -11,21 +11,21 @@
   };
 
   outputs = { nixpkgs, unstable, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      unstablePkgs = import unstable { inherit system; };
-    in {
-      homeConfigurations."marcos" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    unstablePkgs = import unstable { inherit system; };
+  in {
+    homeConfigurations."marcos" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [ ./home.nix ];
 
-        extraSpecialArgs = {
-          inherit unstablePkgs;
-        };
+      extraSpecialArgs = {
+        inherit unstablePkgs;
       };
     };
+  };
 }
