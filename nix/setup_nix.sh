860048e66113b64a094c21d8bc6c93593527e22d
Marcos Fonseca <marco12.fonseca@hotmail.com>
2025-08-11 15:25:58 -0300

    link simbolico para flake.lock

--- nix/setup_nix.sh
+++ nix/setup_nix.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 mkdir -p $HOME/.config/nix
 ln -f nix.conf $HOME/.config/nix/nix.conf
 
 mkdir -p $HOME/.config/home-manager
 ln -f home.nix $HOME/.config/home-manager/home.nix
 ln -f flake.nix $HOME/.config/home-manager/flake.nix
+ln -f flake.lock $HOME/.config/home-manager/flake.lock
 
 if ! command -v nix &> /dev/null; then
   sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon # multi user install
