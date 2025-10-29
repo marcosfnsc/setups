13f03eba8851678db2db457e9b4b422d58ad1c4c
Marcos Fonseca <marco12.fonseca@hotmail.com>
2025-10-28 21:46:32 -0300

    script simplificado

--- nix/setup_nix.sh
+++ nix/setup_nix.sh
@@ -14,13 +14,6 @@ if ! command -v nix &> /dev/null; then
   sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon # multi user install
 fi
 
-if ! command -v home-manager &> /dev/null; then
-  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-  nix-channel --update
-  nix-shell '<home-manager>' -A install
-fi
-
-#home-manager switch
-nix run ~/.config/home-manager#homeConfigurations.marcos.activationPackage
+nix run nixpkgs#home-manager -- switch
 
 ln -f $HOME/.config/home-manager/flake.lock flake.lock
