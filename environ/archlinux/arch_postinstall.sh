6ba1904d66a8c7204a816173f9b787699f6df0ab
Marcos Fonseca <marco12.fonseca@hotmail.com>
2026-05-17 21:23:15 -0300

    corrigido nome do pacote

--- environ/archlinux/arch_postinstall.sh
+++ environ/archlinux/arch_postinstall.sh
@@ -24,7 +24,7 @@ APPS_INSTALL=(
   kid3-qt
   kitty
   konsole
-  krunner5
+  krunner
   libreoffice-fresh # suite office
   libreoffice-fresh-pt-br # pacote de idioma pt-br para o libreoffice
   man-db
